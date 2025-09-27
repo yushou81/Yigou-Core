6502713e75ca4d526f4d0989b62a26983da71126
shiwen <2911627353@qq.com>
2025-09-27 18:51:00 +0800

    1

---
+++ my-electron-app-fixed/frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello from Electron + React 🚀</h1>
+      <button onClick={() => alert('按钮被点击了！')}>点我</button>
+    </div>
+  );
+}
+export default App;
---
+++ my-electron-app-fixed/main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  });
+
+  if (process.env.NODE_ENV === 'development') {
+    win.loadURL('http://localhost:5173');
+  } else {
+    win.loadFile(path.join(__dirname, 'frontend/dist/index.html'));
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
