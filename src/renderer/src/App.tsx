e885a47a224ee1964ec0ad8ef973ed37a62019fd
shiwen <2911627353@qq.com>
2025-11-07 10:59:05 +0800

    文件管理

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -2,43 +2,18 @@
 //import Canvas from './components/canvas/Canvas'
 //import HomePage from './pages/HomePage/HomePage'
 import React from 'react';
-import version
 import { RouterProvider } from 'react-router-dom'
-
+import'./App.css'
 import router from '@renderer/router/router'
 
 function App(): React.JSX.Element {
-  const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
+  //const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
 
   return (
-    <>
       <div className="App">
         <RouterProvider router={router} />
       </div>
 
-      <div className="creator">Powered by electron-vite</div>
-      <div className="text">
-        Build an Electron app with <span className="react">React</span>
-        &nbsp;and <span className="ts">TypeScript</span>
-      </div>
-      <p className="tip">
-        Please try pressing <code>F12</code> to open the devTool
-      </p>
-      <div className="actions">
-        <div className="action">
-          <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
-            Documentation
-          </a>
-        </div>
-        <div className="action">
-          <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
-            Send IPC
-          </a>
-        </div>
-      </div>
-      <Versions></Versions>
-
-    </>
   )
 }
 
